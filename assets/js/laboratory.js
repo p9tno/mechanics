cdc7ccfb9835d34eb9e2ddebc8ed55541b7c98c5
p9tno <plahotin_s@mail.ru>
2023-12-21 14:43:17 +0300

    clone

---
+++ assets/js/laboratory.js
@@ -0,0 +1,81 @@
+
+$(document).ready(function() {
+
+    let total = 0;
+
+    $('.laboratory__page').each(function(indexInArray, valueOfElement) {
+        let questionBlock = indexInArray + 1
+        $(this).attr('data-p', questionBlock);
+        total = total + 1;
+        $('.laboratory__step_total').text(total);
+    });
+
+    let lineStep = 100 / total;
+    let line;
+    let pageNumber = 1;
+    lineWidth();
+
+    $('.laboratory__arrow_next').click(function() {
+
+        $('.laboratory__arrow_prew').css('display', 'flex');
+
+        if (pageNumber < total) {
+            pageNumber++;
+            changeActivePage();
+            playLottie();
+            lineWidth();
+            changeButtonNext();
+        }
+    });
+
+    $('.laboratory__arrow_prew').click(function() {
+        if (pageNumber > 2) {
+            pageNumber--;
+
+            changeActivePage();
+            changeButtonNext();
+
+
+        } else if (pageNumber == 2) {
+            pageNumber--;
+            changeActivePage();
+            $('.laboratory__arrow_prew').hide();
+        }
+        playLottie();
+        lineWidth()
+    });
+
+    function changeActivePage() {
+        $('.laboratory__page.active').hide();
+        $('.laboratory__page.active').removeClass('active');
+        $('.laboratory__page[data-p=' + pageNumber + ']').fadeIn('slow')
+        $('.laboratory__page[data-p=' + pageNumber + ']').addClass('active');
+    }
+
+
+    function lineWidth() {
+        $('.laboratory__step_current').text(pageNumber);
+        line = lineStep * (pageNumber);
+        line = 'calc(' + line + '% )';
+        $('.laboratory__line_bg').css('width', line)
+    }
+
+    function playLottie() {
+        let id = $('.laboratory__page[data-p=' + pageNumber + ']').find('.preview').attr('id');
+        console.log(id);
+        lottie.stop();
+        setTimeout(() => {
+            lottie.play(id);
+        }, 500);
+    }
+
+    function changeButtonNext() {
+        if (pageNumber == total) {
+            $('.laboratory__arrow_next').removeClass('active');
+        } else {
+            $('.laboratory__arrow_next').addClass('active')
+        }
+    }
+
+
+});
---
+++ labs/lab03/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    let path = 'json/';
+
+    function initLabImage() {
+        let lab1 = bodymovin.loadAnimation({
+            container: document.getElementById('lab1'), // Required
+            path: path + 'lab1.json', // Required
+            renderer: 'svg', // Required
+            loop: false, // Optional
+            autoplay: false, // Optional
+            name: "lab1",
+        })
+        let lab2 = bodymovin.loadAnimation({
+            container: document.getElementById('lab2'), // Required
+            path: path + 'lab2.json', // Required
+            renderer: 'svg', // Required
+            loop: false, // Optional
+            autoplay: false, // Optional
+            name: "lab2",
+        })
+        let lab3 = bodymovin.loadAnimation({
+            container: document.getElementById('lab3'), // Required
+            path: path + 'lab3.json', // Required
+            renderer: 'svg', // Required
+            loop: false, // Optional
+            autoplay: false, // Optional
+            name: "lab3",
+        })
+    }
+    initLabImage();
+})
